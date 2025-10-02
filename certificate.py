14136c718d44a1ae1ba550e7563861f0988c8609
janoantelo7 <jano.anteloabelenda@gmail.com>
2025-10-02 21:19:20 +0200

    refactor: move certificate retrieval to __init__ method

--- certificate.py
+++ certificate.py
@@ -6,13 +6,14 @@ class Certifiacate:
     def __init__(self, hostname):
         self.hostname = hostname
         self.context = ssl.create_default_context()
-    
-    def get_expiry_date(self):
+
         with socket.create_connection((self.hostname, 443)) as sock:
             with self.context.wrap_socket(sock, server_hostname=self.hostname) as ssock:
-                cert = ssock.getpeercert()
-                expiry_date = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
-                return expiry_date
+                self.cert = ssock.getpeercert()
+
+    def get_expiry_date(self):
+        expiry_date = datetime.datetime.strptime(self.cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
+        return expiry_date
     
     def days_until_expiration(self):
         expiry_date = self.get_expiry_date()
